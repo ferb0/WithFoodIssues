a4f0433edba4c7861f66818a262df9135d7fd70a
ferb0 <ferb@e.email>
2023-10-24 12:18:01 -0300

    Se agrega detalle de los resultados.

---
+++ src/app/detail/[idResult]/page.jsx
@@ -0,0 +1,30 @@
+"use client"
+import { usePathname } from 'next/navigation'
+import { useEffect } from 'react';
+import { cleanRecepiesInformation, searchRecepiesInformation } from '@/redux/actions';
+import { useDispatch, useSelector } from 'react-redux';
+import { Typography } from '@mui/material';
+
+export default function DetailResult() {
+    const dispatch = useDispatch()
+    const path = usePathname()
+    // Tengo que limpiar el path para solo quedarme con el id.
+    const id = path.split('/').slice(-1)[0]
+
+    const information = useSelector(state => state.recepieInfo)
+
+    useEffect(() => {
+        dispatch(searchRecepiesInformation(id))
+        return function () {
+            dispatch(cleanRecepiesInformation())
+        }
+    }, [dispatch, id])
+
+    return (
+        <div>
+            <h2>{information.title}</h2>
+            <Typography 
+            dangerouslySetInnerHTML={{ __html: information.summary }} />
+        </div>
+    )
+}
