33273288702086a2a4c70d30a36ca0a0c212bb2d
ferb0 <ferb@e.email>
2023-10-25 16:18:50 -0300

    Se cambia action para que tome de esa nueva ruta.

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -2,17 +2,20 @@ export const GET_RESULT_INGREDIENTS = 'GET_RESULT_INGREDIENTS'
 export const GET_RESULT_INFORMATION = 'GET_RESULT_INFORMATION'
 export const CLEAN_RESULT_INFORMATION = 'CLEAN_RESULT_INFORMATION'
 
-const URL = process.env.NEXT_PUBLIC_API_BASE_URL
-const API_KEY = process.env.NEXT_PUBLIC_API_KEY
-
-let requestGet = {
-    headers: { 'x-api-key': API_KEY },
-    method: 'GET'
-}
-
 export const searchRecepiesByIngredient = (ingredient) => {
+    // return function (dispatch) {
+    //     return fetch(URL + `/recipes/complexSearch?includeIngredients=${ingredient}`, requestGet)
+    //         .then(response => {
+    //             if (!response.ok) throw Error(response.status)
+    //             return response.json()
+    //         })
+    //         .then((response) => {
+    //             dispatch({ type: GET_RESULT_INGREDIENTS, payload: response })
+    //         })
+    //         .catch(() => dispatch({ type: GET_RESULT_INGREDIENTS, payload: false }));
+    // }
     return function (dispatch) {
-        return fetch(URL + `/recipes/complexSearch?includeIngredients=${ingredient}`, requestGet)
+        return fetch(`/api/recipes?Ingredients=${ingredient}`)
             .then(response => {
                 if (!response.ok) throw Error(response.status)
                 return response.json()
