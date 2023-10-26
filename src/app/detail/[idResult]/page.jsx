4b3fa75f73a72a3c529c5e065a3d8c61769b2eec
ferb0 <ferb@e.email>
2023-10-26 12:48:37 -0300

    Se quita redux, después se completará.

--- src/app/detail/[idResult]/page.jsx
+++ src/app/detail/[idResult]/page.jsx
@@ -1,24 +1,23 @@
-"use client"
+'use client'
 import { usePathname } from 'next/navigation'
-import { useEffect } from 'react';
-import { cleanRecepiesInformation, searchRecepiesInformation } from '@/redux/actions';
-import { useDispatch, useSelector } from 'react-redux';
+// import { useEffect } from 'react';
+// import { cleanRecepiesInformation, searchRecepiesInformation } from '@/redux/actions';
+//import { useDispatch, useSelector } from 'react-redux';
 import { Typography } from '@mui/material';
 
 export default function DetailResult() {
-    const dispatch = useDispatch()
     const path = usePathname()
     // Tengo que limpiar el path para solo quedarme con el id.
     const id = path.split('/').slice(-1)[0]
 
-    const information = useSelector(state => state.recepieInfo)
+    //const information = useSelector(state => state.recepieInfo)
 
-    useEffect(() => {
-        dispatch(searchRecepiesInformation(id))
-        return function () {
-            dispatch(cleanRecepiesInformation())
-        }
-    }, [dispatch, id])
+    // useEffect(() => {
+    //     dispatch(searchRecepiesInformation(id))
+    //     return function () {
+    //         dispatch(cleanRecepiesInformation())
+    //     }
+    // }, [dispatch, id])
 
     return (
         <div>
