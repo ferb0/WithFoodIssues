5315897d1288eca000dd70e04bbe842d60846b79
ferb0 <ferb@e.email>
2023-10-27 10:18:53 -0300

    Pequeño cambio en la query ingredientes y se agrega offset.

--- src/app/api/search_recipes/route.js
+++ src/app/api/search_recipes/route.js
@@ -4,10 +4,11 @@ const URL_API = process.env.API_BASE_URL
 
 export async function GET(request) {
     const { searchParams } = new URL(request.url)
-    const ingredients = searchParams.get('Ingredients')
+    const ingredients = searchParams.get('ingredients')
+    const offSet = searchParams.get('offSet')
 
     try {
-        const response = await fetch(URL_API + `/recipes/complexSearch?includeIngredients=${ingredients}`, requestGet)
+        const response = await fetch(URL_API + `/recipes/complexSearch?includeIngredients=${ingredients}&offset=${offSet}`, requestGet)
         const data = await response.json()
 
         return Response.json(data)
