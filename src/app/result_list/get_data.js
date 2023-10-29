ccf44fc4b95c0b408ad4024eed0c6b648e994564
ferb0 <ferb@e.email>
2023-10-29 17:59:09 -0300

    Se agrega recordatorio.

--- src/app/result_list/get_data.js
+++ src/app/result_list/get_data.js
@@ -1,6 +1,8 @@
 export default function getRecipes(search, setResults, offSet = 0, options = {}) {
     let URL = `/api/search_recipes?ingredients=${search}&offSet=${offSet}`
 
+    // Agregar cheks de opciones
+    
     for (const property in options) {
         if (options[property])
             URL += `&${property}=${options[property]}`
