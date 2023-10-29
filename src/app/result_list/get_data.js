67a21a205e415044cb828026eed328bc9aad0b02
ferb0 <ferb@e.email>
2023-10-29 18:09:17 -0300

    Se hace correccion en error.

--- src/app/result_list/get_data.js
+++ src/app/result_list/get_data.js
@@ -2,7 +2,7 @@ export default function getRecipes(search, setResults, offSet = 0, options = {})
     let URL = `/api/search_recipes?ingredients=${search}&offSet=${offSet}`
 
     // Agregar cheks de opciones
-    
+
     for (const property in options) {
         if (options[property])
             URL += `&${property}=${options[property]}`
@@ -15,5 +15,5 @@ export default function getRecipes(search, setResults, offSet = 0, options = {})
     //         return response.json()
     //     })
     //     .then((response) => setResults(response))
-    //     .catch(() => setResults([]))
+    //     .catch(() => setResults({ results: [] }))
 }
