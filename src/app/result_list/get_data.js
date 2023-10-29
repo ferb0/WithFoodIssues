642bda33844774c018636a50ae7b8f5f3d8ee491
ferb0 <ferb@e.email>
2023-10-29 17:13:23 -0300

    Se comenta peticion para hacer pruebas del string.

--- src/app/result_list/get_data.js
+++ src/app/result_list/get_data.js
@@ -1,9 +1,17 @@
-export default function getRecipes(search, setResults, offSet = 0) {
-    fetch(`/api/search_recipes?ingredients=${search}&offSet=${offSet}`)
-        .then(response => {
-            if (!response.ok) throw Error(response.status)
-            return response.json()
-        })
-        .then((response) => setResults(response))
-        .catch(() => setResults([]))
+export default function getRecipes(search, setResults, offSet = 0, options = {}) {
+    let URL = `/api/search_recipes?ingredients=${search}&offSet=${offSet}`
+
+    for (const property in options) {
+        if (options[property])
+            URL += `&${property}=${options[property]}`
+    }
+
+    console.log('URL', URL)
+    // fetch(URL)
+    //     .then(response => {
+    //         if (!response.ok) throw Error(response.status)
+    //         return response.json()
+    //     })
+    //     .then((response) => setResults(response))
+    //     .catch(() => setResults([]))
 }
