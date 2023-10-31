82fb886754b7bfadc97d423b546aecc80f215857
ferb0 <ferb@e.email>
2023-10-31 11:40:57 -0300

    Se centra animación.

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -4,7 +4,13 @@ import CircularProgress from '@mui/material/CircularProgress'
 
 export default function Loading() {
     return (
-        <Stack spacing={1} >
+        <Stack
+            spacing={3}
+            sx={{
+                paddingTop: '1rem',
+                display: 'block',
+                textAlign: 'center'
+            }} >
             <CircularProgress />
             <Typography>Loading...</Typography>
         </Stack>
