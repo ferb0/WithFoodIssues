209b3864d07d384ab70d647862c184894f4edb6f
ferb0 <ferb@e.email>
2023-10-31 08:50:41 -0300

    Se agrega control de cuisines.

--- src/app/result_list/options_control.js
+++ src/app/result_list/options_control.js
@@ -1,8 +1,9 @@
-import { dietOptions, intolerancesOptions } from "@/global_objects/recipes_options"
+import { dietOptions, intolerancesOptions, cuisineOptions } from "@/global_objects/recipes_options"
 
 export const optionsControl = (options) => {
-    if ((dietOptions.includes(options.diet) || options.diet === '' ) &&
-        (intolerancesOptions.includes(options.intolerances) || options.intolerances === ''))
+    if ((dietOptions.includes(options.diet) || options.diet === '') &&
+        (intolerancesOptions.includes(options.intolerances) || options.intolerances === '') &&
+        (cuisineOptions.includes(options.cuisine) || options.cuisine === ''))
         return options
     return {}
 }
