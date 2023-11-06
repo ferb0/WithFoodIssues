90ff19aa7bba21576f74ead03ce73e0904ceb990
ferb0 <ferb@e.email>
2023-11-06 16:17:29 -0300

    Se quita useContext y se usa params para busqueda.

--- src/app/results_list/page.jsx
+++ src/app/results_list/page.jsx
@@ -1,45 +1,32 @@
 'use client'
 
-import { useContext, useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { useSearchParams, useRouter } from 'next/navigation'
 
 import { Container, Grid, Pagination } from '@mui/material'
 
 import Results from './results.jsx'
 
-import { searchContext } from '@/context/search_context.js'
-
 import { getRecipes } from './_controllers/get_data.js'
 
 export default function ResultList() {
-    let { search,
-        results, setResults,
-        dietOption,
-        intolerancesOption,
-        cuisineOption
-    } = useContext(searchContext)
-
-    const options = {
-        diet: dietOption,
-        intolerances: intolerancesOption,
-        cuisine: cuisineOption
-    }
-
     const router = useRouter()
     const searchParams = useSearchParams()
 
-    let [page, setPage] = useState(searchParams.get('page'))
+    let [results, setResults] = useState([[]])
+    console.log(results)
+    const page = searchParams.get('page')
+    const search = searchParams.get('search')
 
     const changePage = (event, value) => {
-        setPage(value)
         getRecipes(search, setResults, value * results.number)
-        router.push(`?page=${value}`)
+        router.push(`&page=${value}`)
     }
 
     useEffect(() => {
         if (search)
-            getRecipes(search, setResults, results.number ? page * results.number : 0, options)
-    }, [search, dietOption, intolerancesOption, cuisineOption])
+            getRecipes(search).then(res => setResults(res))
+    }, [])
 
     return (
         <Container sx={{ padding: '1rem' }}>
@@ -54,7 +41,7 @@ export default function ResultList() {
                         <Results key={el.id} element={el} />
                     </Grid>)}
             </Grid>
-            {results.length != 0 ?
+            {/* {results.length != 0 ?
                 <Pagination
                     count={Math.ceil(results.totalResults / results.number)}
                     page={page ? parseInt(page) : 1}
@@ -65,7 +52,7 @@ export default function ResultList() {
                         justifyContent: 'center'
                     }} />
                 :
-                null}
+                null} */}
         </Container>
     )
 }
