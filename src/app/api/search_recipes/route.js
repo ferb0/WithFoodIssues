9d00aae16f3adddf6677f31e1a1a63cc8d1e702a
ferb0 <ferb@e.email>
2023-11-28 11:11:12 -0300

    Se agregan opciones de busqueda en la api.

--- src/app/api/search_recipes/route.js
+++ src/app/api/search_recipes/route.js
@@ -6,8 +6,19 @@ export async function GET(request) {
     const ingredients = getParams(request, 'ingredients')
     const offSet = getParams(request, 'offSet')
 
+    let options = ''
+    const diet = getParams(request, 'diet')
+    const intolerances = getParams(request, 'intolerances')
+    const cuisine = getParams(request, 'cuisine')
+    if (diet)
+        options += `&diet=${diet}`
+    if (intolerances)
+        options += `&intolerances=${intolerances}`
+    if (cuisine)
+        options += `&cuisine=${cuisine}`
+
     try {
-        const response = await fetch(URL_API + `/recipes/complexSearch?includeIngredients=${ingredients}&offset=${offSet}`, requestGet)
+        const response = await fetch(URL_API + `/recipes/complexSearch?includeIngredients=${ingredients}&offset=${offSet}` + options, requestGet)
         const data = await response.json()
 
         return Response.json(data)
