68bd284f1a40ce5bf67fe9e24e7c9d0c15328c9e
ferb0 <ferb@e.email>
2023-11-29 11:48:46 -0300

    Se agrega Resume a los detalles.

--- src/app/detail/resume.jsx
+++ src/app/detail/resume.jsx
@@ -1,21 +1,34 @@
-import { Typography, CardMedia, Card, Grid } from '@mui/material'
+import { Typography, CardMedia, Card, Grid, Stack, Link } from '@mui/material'
 
-export const ResumeDetail = ({ image, summary }) => {
+export const ResumeDetail = ({ image, summary, source }) => {
     return (
         <Card
             sx={{ padding: '0.5rem' }}>
             <Grid container spacing={2}>
-                <Grid item xs={4}>
-                    <CardMedia
-                        component='img'
-                        src={image}
-                        alt='Image recipe' />
 
+                <Grid item xs={4}>
+                    <Stack>
+                        <CardMedia
+                            component='img'
+                            src={image}
+                            alt='Image recipe' />
+                        <Link
+                            href={source ? 'https://www.' + source : ''}
+                            target='_blank'
+                            underline="none"
+                            rel="noreferrer"
+                            sx={{ display: 'flex', margin: 'auto' }}>
+                            {source}
+                        </Link>
+
+                    </Stack>
                 </Grid>
+
                 <Grid item xs={8}>
                     <Typography
                         dangerouslySetInnerHTML={{ __html: summary }} />
                 </Grid>
+
             </Grid>
         </Card>
     )
