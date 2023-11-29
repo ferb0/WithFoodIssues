5f1a953953155b93f4a4d89b3bb65fecd35018d3
ferb0 <ferb@e.email>
2023-11-29 15:41:51 -0300

    Se agrega recuadro de inforamación nutricional.

---
+++ src/app/detail/_nutrition_element/nutrient.jsx
@@ -0,0 +1,28 @@
+import { Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from "@mui/material"
+
+export const Nutrients = ({ infoNutrients }) => {
+    return (
+        <TableContainer
+            size='small'
+            component={Paper}
+            sx={{ width: 'auto', padding: '1rem', margin: 'auto', marginBottom: '1rem' }}>
+            <Table sx={{ maxWidth: 400 }}>
+                <TableHead>
+                    <TableRow>
+                        <TableCell align='center'>Name</TableCell>
+                        <TableCell align='center'>Amount</TableCell>
+                        <TableCell align='center'>Percent Of Daily Needs</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {infoNutrients?.map(elem =>
+                        <TableRow key={elem.name} sx={{ border: 0 }}>
+                            <TableCell align='center'>{elem.name}</TableCell>
+                            <TableCell align='center'>{elem.amount} {elem.unit}</TableCell>
+                            <TableCell align='center'>{elem.percentOfDailyNeeds}</TableCell>
+                        </TableRow>)}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    )
+}
--- src/app/detail/nutritions_detail.jsx
+++ src/app/detail/nutritions_detail.jsx
@@ -1,16 +1,20 @@
 import { Card, Typography, Stack } from "@mui/material"
-import { CaloriesBreackDown } from "./_nutrition_element/calories_breackdown"
+import { CaloriesBreackDown } from "./_nutrition_element/calories_breackdown.jsx"
+import { Nutrients } from "./_nutrition_element/nutrient.jsx"
 
 export const NutritionsDetails = ({ nutrition }) => {
+    console.log(nutrition)
     return (
         <Card>
             <Typography
                 variant='h5'
-                sx={{ textAlign: 'center' }}>
+                sx={{ textAlign: 'center' }}
+                pb='1rem'>
                 Nutritional Facts
             </Typography>
 
-            <Stack>
+            <Stack direction='row'>
+                <Nutrients infoNutrients={nutrition?.nutrients}/>
                 <CaloriesBreackDown dataCalories={nutrition?.caloricBreakdown} />
             </Stack>
         </Card>
