995846b9561ec3e7309026e94d63fb85efae2a3b
ferb0 <ferb@e.email>
2023-11-29 16:15:22 -0300

    Se cmbia Stack por Table.

--- src/app/detail/_nutrition_element/calories_breackdown.jsx
+++ src/app/detail/_nutrition_element/calories_breackdown.jsx
@@ -1,12 +1,19 @@
-import { Stack, Typography } from "@mui/material"
+import { Table, TableBody, TableCell, TableContainer, TableRow, Paper } from "@mui/material"
 
 export const CaloriesBreackDown = ({ dataCalories }) => {
     const PADDING_SIDE = '3rem'
     return (
-        <Stack direction='row'>
-            <Typography pl={PADDING_SIDE} mr='auto'>Carbs: {dataCalories?.percentCarbs}%.</Typography>
-            <Typography m='auto'>Fat: {dataCalories?.percentFat}%.</Typography>
-            <Typography pr={PADDING_SIDE} ml='auto'>Protein: {dataCalories?.percentProtein}%.</Typography>
-        </Stack>
+        <TableContainer
+            size='small'
+            component={Paper}
+            sx={{ width: 'auto', padding: '1rem', margin: 'auto' }}>
+            <Table sx={{ maxWidth: 400 }}>
+                <TableBody>
+                    <TableRow sx={{ border: 0 }}><TableCell>Carbs: {dataCalories?.percentCarbs}%.</TableCell></TableRow>
+                    <TableRow sx={{ border: 0 }}><TableCell>Fat: {dataCalories?.percentFat}%.</TableCell></TableRow>
+                    <TableRow sx={{ border: 0 }}><TableCell>Protein: {dataCalories?.percentProtein}%.</TableCell></TableRow>
+                </TableBody>
+            </Table>
+        </TableContainer>
     )
 }
