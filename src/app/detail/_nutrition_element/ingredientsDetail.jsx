7c860eedfb6d8ee4cc882a0d31ebe3e8c34625ba
ferb0 <ferb@e.email>
2023-11-30 15:12:03 -0300

    Se agregan headres.

--- src/app/detail/_nutrition_element/ingredientsDetail.jsx
+++ src/app/detail/_nutrition_element/ingredientsDetail.jsx
@@ -1,21 +1,26 @@
-import { Typography, Card, Stack, TableHead } from '@mui/material'
-import { Table, TableBody, TableCell, TableContainer, TableRow, Paper } from "@mui/material"
+import { Table, TableBody, TableCell, TableContainer, TableRow, Paper, TableHead } from "@mui/material"
 
 export const IngredientsDetail = ({ ingredientsArray }) => {
     return (
-                <TableContainer
-                    size='small'
-                    component={Paper}
-                    sx={{ width: 'auto', padding: '1rem', margin: 'auto', marginTop: '0rem' }}>
-                    <Table sx={{ maxWidth: 400 }}>
-                        <TableBody>
-                            {ingredientsArray?.map(element =>
-                                <TableRow key={element.id} sx={{ border: 0 }}>
-                                    <TableCell>{element.name[0].toUpperCase() + element.name.substring(1)}</TableCell>
-                                    <TableCell> {element.measures.metric.amount + ' ' + element.measures.metric.unitShort}</TableCell>
-                                </TableRow>)}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
+        <TableContainer
+            size='small'
+            component={Paper}
+            sx={{ width: 'auto', padding: '1rem', margin: 'auto', marginTop: '0rem' }}>
+            <Table sx={{ maxWidth: 400 }}>
+                <TableHead>
+                    <TableRow>
+                        <TableCell><strong>Ingredients</strong></TableCell>
+                        <TableCell><strong>Amount</strong></TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {ingredientsArray?.map(element =>
+                        <TableRow key={element.id} sx={{ border: 0 }}>
+                            <TableCell>{element.name[0].toUpperCase() + element.name.substring(1)}</TableCell>
+                            <TableCell> {element.measures.metric.amount + ' ' + element.measures.metric.unitShort}</TableCell>
+                        </TableRow>)}
+                </TableBody>
+            </Table>
+        </TableContainer>
     )
 }
