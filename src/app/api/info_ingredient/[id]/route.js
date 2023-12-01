34fd4b90f0bef7bb348eba19baa160075a78e185
ferb0 <ferb@e.email>
2023-12-01 16:20:03 -0300

    Se agrega ruta para obtencion de info de ingrediente.

---
+++ src/app/api/info_ingredient/[id]/route.js
@@ -0,0 +1,16 @@
+const URL_API = process.env.API_BASE_URL
+import { requestGet } from '../../requestAPI.js'
+
+export async function GET(request, { params }) {
+    const { id } = params
+
+    try {
+        const response = await fetch(URL_API + `/food/ingredients/${id}/information`, requestGet)
+        const data = await response.json()
+
+        return Response.json({ data })
+    }
+    catch (error) {
+        return Response.json(error)
+    }
+}
