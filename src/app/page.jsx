63cda67a24aa9560ddca9ba1a6a1a8b41693b9ac
ferb0 <ferb@e.email>
2023-12-01 13:04:29 -0300

    Se cambia forma de ejecución landing.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,10 @@
+import Landing from "./landing_page.jsx"
 import ResultList from "./results_list/page.jsx"
 
 export default function Home() {
   return (
     <main>
+      <Landing />
       <ResultList />
     </main>
   )
--- src/app/results_list/page.jsx
+++ src/app/results_list/page.jsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { useRouter } from 'next/navigation'
 import { useState, useEffect, useContext } from 'react'
 import { useSearchParams } from 'next/navigation'
 
@@ -10,14 +11,13 @@ import Results from './results.jsx'
 
 import { getRecipes } from './_controllers/get_data.js'
 import { searchContext } from '../../context/search_context.js'
-import { Landing } from '../landing_page.jsx'
 
 export default function ResultList() {
+    const route = useRouter()
     const searchParams = useSearchParams()
+    const search = searchParams.get('search')
 
     let [results, setResults] = useState({ results: [] })
-
-    const search = searchParams.get('search')
 
     const {
         dietOption,
@@ -26,32 +26,30 @@ export default function ResultList() {
     } = useContext(searchContext)
 
     useEffect(() => {
-        if (search)
+        if (search) {
             getRecipes(search, { diet: dietOption, intolerances: intolerancesOption, cuisine: cuisineOption }).then(res => setResults(res))
+            route.push(`/results_list?search=${search}`)
+        }
     }, [search, dietOption, intolerancesOption, cuisineOption])
 
     return (
-        <>
-            {results.results.length == 0 ?
-                <Landing /> :
-                <Container sx={{ padding: '1rem' }}>
+        <Container sx={{ padding: '1rem' }}>
+            <Grid
+                container
+                spacing={2}>
+                {results.results?.map(el =>
                     <Grid
-                        container
-                        spacing={2}>
-                        {results.results?.map(el =>
-                            <Grid
-                                item
-                                key={el.id}
-                                xs={12} sm={6} md={4} lg={4}>
-                                <Results key={el.id} element={el} />
-                            </Grid>)}
-                    </Grid>
+                        item
+                        key={el.id}
+                        xs={12} sm={6} md={4} lg={4}>
+                        <Results key={el.id} element={el} />
+                    </Grid>)}
+            </Grid>
 
-                    <ResultsPagination
-                        number={results.number}
-                        totalResults={results.totalResults}
-                        setResults={setResults} />
-                </Container>}
-        </>
+            <ResultsPagination
+                number={results.number}
+                totalResults={results.totalResults}
+                setResults={setResults} />
+        </Container>
     )
 }
