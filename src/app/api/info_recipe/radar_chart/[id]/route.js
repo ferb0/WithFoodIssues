15fc6c754c1c03b29528950ef12aee4ac571430a
ferb0 <ferb@e.email>
2023-12-05 10:05:26 -0300

    Se agrega ruta para obtener info sobre el sabor.

---
+++ src/app/api/info_recipe/radar_chart/[id]/route.js
@@ -0,0 +1,16 @@
+const URL_API = process.env.API_BASE_URL
+import { requestGet } from '../../../requestAPI.js'
+
+export async function GET(request, { params }) {
+    const { id } = params
+
+    try {
+        const response = await fetch(URL_API + `/recipes/${id}/tasteWidget.json`, requestGet)
+        const data = await response.json()
+
+        return Response.json({ data })
+    }
+    catch (error) {
+        return Response.json(error)
+    }
+}
