7b573ba2d46b750196775399a45c9c635fe402c3
ferb0 <ferb@e.email>
2023-12-05 10:07:06 -0300

    Se crea componente para mostrar el grafico.

---
+++ src/app/(details)/_common/_nutrition_element/taste_graphic_chart.jsx
@@ -0,0 +1,36 @@
+import React from "react"
+
+import RadarChart from 'react-svg-radar-chart';
+import 'react-svg-radar-chart/build/css/index.css'
+import { formateChart } from "./formated_data_chart";
+import { Card } from "@mui/material";
+
+const captions = {
+    // columns
+    bitterness: 'Bitterness',
+    fattiness: 'Fattiness',
+    saltiness: 'Saltiness',
+    savoriness: 'Savoriness',
+    sourness: 'Sourness',
+    spiciness: 'Spiciness',
+    sweetness: 'Sweetness'
+}
+
+export default function TasteChart({ idRecipe }) {
+    const [dataTaste, setDataTaste] = React.useState([])
+
+    React.useEffect(() => {
+        fetch(`/api/info_recipe/radar_chart/${idRecipe}`)
+            .then(res => res.json())
+            .then(res =>
+                setDataTaste([{ data: formateChart(res.data), meta: { color: 'orange' } }]))
+    }, [])
+
+    return (
+        <Card sx={{ display: 'flex', margin: 'auto', marginBottom: { xs: '1rem', sm: '1rem' } }}>
+            <RadarChart
+                captions={captions}
+                data={dataTaste} />
+        </Card>
+    )
+}
