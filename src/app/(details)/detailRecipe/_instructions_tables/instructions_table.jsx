b84610ad0d9ae819070789e3f1ae54e14d17af21
ferb0 <ferb@e.email>
2023-12-08 19:08:16 -0300

    Se agrega imagenes a las tablas de ingredientes y utencillos.

--- src/app/(details)/detailRecipe/_instructions_tables/instructions_table.jsx
+++ src/app/(details)/detailRecipe/_instructions_tables/instructions_table.jsx
@@ -1,6 +1,10 @@
-import { Card, TableContainer, Table, TableBody, TableRow, Paper, TableCell, TableHead } from '@mui/material'
+import { Card, TableContainer, Table, TableBody, TableRow, Paper, TableCell, TableHead, Typography } from '@mui/material'
+import Image from 'next/image'
+
+import { imageBlurResult } from '@/global_objects/image_blur_result.js'
 
 export default function InstructionsTable({ elements, nameTable }) {
+    const SIZE = '100x100'
     return (
         <Card sx={{ margin: 'auto' }}>
             <TableContainer
@@ -15,7 +19,18 @@ export default function InstructionsTable({ elements, nameTable }) {
                     </TableHead>
                     <TableBody>
                         {elements?.map(element =>
-                            <TableRow key={element.id + element.name} sx={{ border: 0 }}><TableCell>{element.name}</TableCell></TableRow>)}
+                            <TableRow key={element.id + element.name} sx={{ border: 0 }}>
+                                <TableCell sx={{textAlign: 'center', display: 'flex'}}>
+                                    <Image
+                                    src={`https://spoonacular.com/cdn/equipment_${SIZE}/${element.name}.jpg`}
+                                    width={'30'}
+                                    height={'30'}
+                                    alt={`${element.name}_image`}
+                                    placeholder='blur'
+                                    blurDataURL={imageBlurResult} />
+                                    <Typography>{element.name}</Typography>
+                                    </TableCell>
+                                </TableRow>)}
                     </TableBody>
                 </Table>
             </TableContainer>
