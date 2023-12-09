a4fd1edd7d03e7cdb71b53822ed1be3f0f8f2643
ferb0 <ferb@e.email>
2023-12-09 17:46:59 -0300

    Se agrega Skeleton en Nutrients.

--- src/app/(details)/_common/_nutrition_element/nutrient.jsx
+++ src/app/(details)/_common/_nutrition_element/nutrient.jsx
@@ -1,29 +1,71 @@
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material'
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Skeleton, Typography } from '@mui/material'
 
 export default function Nutrients({ infoNutrients }) {
+    const skeletonArray = new Array(20).fill(0)
     return (
         <TableContainer
             size='small'
             component={Paper}
             sx={{
-                width: 'auto', padding: '1rem', margin: 'auto', 
-                marginBottom: {sm: '1rem',md:'auto', sm: 'auto', lg: 'auto', xl: 'auto'} }}>
+                width: 'auto', padding: '1rem', margin: 'auto',
+                marginBottom: { sm: '1rem', md: 'auto', sm: 'auto', lg: 'auto', xl: 'auto' }
+            }}>
             <Table sx={{ maxWidth: 400 }}>
                 <TableHead>
                     <TableRow>
-                        <TableCell align='center' sx={{ padding: '0.5rem' }}><strong>Name</strong></TableCell>
-                        <TableCell align='center' sx={{ padding: '0.5rem' }}><strong>Amount</strong></TableCell>
-                        <TableCell align='center' sx={{ padding: '0.5rem' }}><strong>Percent Of Daily Needs</strong></TableCell>
+                        <TableCell align='center' sx={{ padding: '0.5rem' }}>
+                            <Typography variant='h6'>
+                                Name
+                            </Typography>
+                        </TableCell>
+                        <TableCell align='center' sx={{ padding: '0.5rem' }}>
+                            <Typography variant='h6'>
+                                Amount
+                            </Typography>
+                        </TableCell>
+                        <TableCell align='center' sx={{ padding: '0.5rem' }}>
+                            <Typography variant='h6'>
+                                Percent Of Daily Needs
+                            </Typography>
+                        </TableCell>
                     </TableRow>
                 </TableHead>
-                <TableBody>
-                    {infoNutrients?.map(elem =>
-                        <TableRow key={elem.name} sx={{ border: 0 }}>
-                            <TableCell align='center' sx={{ padding: '0.5rem' }}>{elem.name}</TableCell>
-                            <TableCell align='center' sx={{ padding: '0.5rem' }}>{elem.amount} {elem.unit}</TableCell>
-                            <TableCell align='center' sx={{ padding: '0.5rem' }}>{elem.percentOfDailyNeeds} %</TableCell>
-                        </TableRow>)}
-                </TableBody>
+                {infoNutrients ?
+                    <TableBody>
+                        {infoNutrients?.map(elem =>
+                            <TableRow key={elem.name} sx={{ border: 0 }}>
+                                <TableCell align='center' sx={{ padding: '0.5rem' }}>
+                                    <Typography>
+                                        {elem.name}
+                                    </Typography>
+                                </TableCell>
+                                <TableCell align='center' sx={{ padding: '0.5rem' }}>
+                                    <Typography>
+                                        {elem.amount} {elem.unit}
+                                    </Typography>
+                                </TableCell>
+                                <TableCell align='center' sx={{ padding: '0.5rem' }}>
+                                    <Typography>
+                                        {elem.percentOfDailyNeeds} %
+                                    </Typography>
+                                </TableCell>
+                            </TableRow>)}
+                    </TableBody>
+                    :
+                    <TableBody>
+                        {skeletonArray.map(() =>
+                            <TableRow>
+                                <TableCell>
+                                    <Skeleton variant='text' width='6rem' />
+                                </TableCell>
+                                <TableCell>
+                                    <Skeleton variant='text' width='6rem' />
+                                </TableCell>
+                                <TableCell>
+                                    <Skeleton variant='text' width='6rem' />
+                                </TableCell>
+                            </TableRow>)}
+                    </TableBody>}
             </Table>
         </TableContainer>
     )
