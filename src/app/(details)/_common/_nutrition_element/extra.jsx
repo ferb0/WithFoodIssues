209377d1b99cc594d057f9213ebab48b1f56e8bd
ferb0 <ferb@e.email>
2023-12-09 16:38:48 -0300

    Se agrega skeleton a Extra.

--- src/app/(details)/_common/_nutrition_element/extra.jsx
+++ src/app/(details)/_common/_nutrition_element/extra.jsx
@@ -1,4 +1,4 @@
-import { Table, TableBody, TableCell, TableContainer, TableRow, Paper } from '@mui/material'
+import { Table, TableBody, TableCell, TableContainer, TableRow, Paper, Skeleton, Typography } from '@mui/material'
 
 export default function Extra({ properties }) {
     return (
@@ -7,10 +7,37 @@ export default function Extra({ properties }) {
             component={Paper}
             sx={{ width: 'auto', padding: '1rem', margin: 'auto', marginTop: '0.5rem', marginBottom: '1rem' }}>
             <Table sx={{ maxWidth: 400 }}>
-                <TableBody>
-                    {properties?.map(element =>
-                        <TableRow key={element.name} sx={{ border: 0 }}><TableCell>{element.name}: {element.amount.toString().slice(0, 5)}{element.unit}.</TableCell></TableRow>)}
-                </TableBody>
+                {properties ?
+                    <TableBody>
+                        {properties?.map(element =>
+                            <TableRow key={element.name} sx={{ border: 0 }}>
+                                <TableCell>
+                                    <Typography>
+                                        {`${element.name}: ${element.amount.toString().slice(0, 5)}${element.unit}.`}
+                                    </Typography>
+                                </TableCell>
+                            </TableRow>)}
+                    </TableBody>
+                    :
+                    <TableBody>
+                        <TableRow>
+                            <TableCell>
+                                <Skeleton variant="text" width='9rem' />
+                            </TableCell>
+                        </TableRow>
+
+                        <TableRow>
+                            <TableCell>
+                                <Skeleton variant="text" width='9rem' />
+                            </TableCell>
+                        </TableRow>
+
+                        <TableRow>
+                            <TableCell>
+                                <Skeleton variant="text" width='9rem' />
+                            </TableCell>
+                        </TableRow>
+                    </TableBody>}
             </Table>
         </TableContainer>
     )
