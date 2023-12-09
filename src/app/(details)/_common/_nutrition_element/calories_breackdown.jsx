3cd365eb1b3c8e47df92798c3611c8b4ed7aa915
ferb0 <ferb@e.email>
2023-12-09 16:25:38 -0300

    Se agrega skeleton a calories breackdown.

--- src/app/(details)/_common/_nutrition_element/calories_breackdown.jsx
+++ src/app/(details)/_common/_nutrition_element/calories_breackdown.jsx
@@ -1,4 +1,4 @@
-import { Table, TableBody, TableCell, TableContainer, TableRow, Paper } from '@mui/material'
+import { Table, TableBody, TableCell, TableContainer, TableRow, Paper, Typography, Skeleton } from '@mui/material'
 
 export default function CaloriesBreackDown({ dataCalories }) {
     const PADDING_SIDE = '3rem'
@@ -12,9 +12,31 @@ export default function CaloriesBreackDown({ dataCalories }) {
             }}>
             <Table sx={{ maxWidth: 400 }}>
                 <TableBody>
-                    <TableRow sx={{ border: 0 }}><TableCell>Carbs: {dataCalories?.percentCarbs}%.</TableCell></TableRow>
-                    <TableRow sx={{ border: 0 }}><TableCell>Fat: {dataCalories?.percentFat}%.</TableCell></TableRow>
-                    <TableRow sx={{ border: 0 }}><TableCell>Protein: {dataCalories?.percentProtein}%.</TableCell></TableRow>
+
+                    <TableRow sx={{ border: 0 }}>
+                        <TableCell>
+                            <Typography>{dataCalories ? `Protein: ${dataCalories?.percentProtein}%.` : <Skeleton variant="text" width='7rem' />}</Typography>
+                        </TableCell>
+                    </TableRow>
+
+                    <TableRow sx={{ border: 0 }}>
+                        <TableCell>
+                            {dataCalories ?
+                                <Typography>Carbs: {dataCalories?.percentCarbs}%.</Typography>
+                                :
+                                <Skeleton variant="text" width='7rem' />}
+                        </TableCell>
+                    </TableRow>
+
+                    <TableRow sx={{ border: 0 }}>
+                        <TableCell>
+                            {dataCalories ?
+                                <Typography>Fat: {dataCalories?.percentFat}%.</Typography>
+                                :
+                                <Skeleton variant="text" width='7rem' />}
+                        </TableCell>
+                    </TableRow>
+
                 </TableBody>
             </Table>
         </TableContainer>
