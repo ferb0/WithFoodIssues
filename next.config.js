2bf9616a044b0d90f1f033e0f10b444cea6daa6f
ferb0 <ferb@e.email>
2023-12-09 17:55:33 -0300

    Se agrega path de imagen necesario para Next.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'spoonacular.com',
+                port: '',
+                pathname: '/cdn/equipment_250x250/**',
+            },
+        ],
+    },
+}
 
 module.exports = nextConfig
+
