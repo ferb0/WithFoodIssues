6f6873c18c393e146f707e6fdb960b4a8554a37e
ferb0 <ferb@e.email>
2023-12-11 10:13:03 -0300

    Se contenpla posibilidad de cero resultados.

--- src/app/results_list/page.jsx
+++ src/app/results_list/page.jsx
@@ -4,7 +4,7 @@ import { useRouter } from 'next/navigation'
 import { useState, useEffect, useContext } from 'react'
 import { useSearchParams } from 'next/navigation'
 
-import { Container, Grid } from '@mui/material'
+import { Container, Grid, Typography } from '@mui/material'
 
 import ResultsPagination from './results_paginations.jsx'
 import Results from '../_common_components/results.jsx'
@@ -17,7 +17,7 @@ export default function ResultList() {
     const searchParams = useSearchParams()
     const search = searchParams.get('search')
 
-    let [results, setResults] = useState({ results: [] })
+    let [results, setResults] = useState({ results: [], totalResults: null })
 
     const {
         dietOption,
@@ -34,20 +34,27 @@ export default function ResultList() {
 
     return (
         <Container>
-            <Grid container >
-                {results.results?.map(el =>
-                    <Grid
-                        item
-                        key={el.id}
-                        xs={12} sm={6} md={4} lg={3}>
-                        <Results key={el.id} element={el} />
-                    </Grid>)}
-            </Grid>
+            {results.totalResults !== null && results.totalResults === 0 ?
+                <Typography align='center'>
+                    There is not recipes to show with this ingredient.
+                </Typography>
+                :
+                <Container>
+                    <Grid container >
+                        {results.results?.map(el =>
+                            <Grid
+                                item
+                                key={el.id}
+                                xs={12} sm={6} md={4} lg={3}>
+                                <Results key={el.id} element={el} />
+                            </Grid>)}
+                    </Grid>
 
-            <ResultsPagination
-                number={results.number}
-                totalResults={results.totalResults}
-                setResults={setResults} />
+                    <ResultsPagination
+                        number={results.number}
+                        totalResults={results.totalResults}
+                        setResults={setResults} />
+                </Container>}
         </Container>
     )
 }
