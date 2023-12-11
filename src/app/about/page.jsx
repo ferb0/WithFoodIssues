8a7530cf3c4f8142a03e0d0f56bc7fac576907ff
ferb0 <ferb@e.email>
2023-12-11 08:09:28 -0300

    Se re diseña about.

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,10 +1,43 @@
-import { Typography } from '@mui/material'
+import { Typography, Link, Stack } from '@mui/material'
 
 export default function About() {
     return (
-        <div>
-            <h2>About</h2>
-            <Typography>Created by FerB.</Typography>
-        </div>
+        <Stack spacing={2}>
+            <Typography variant='h3' align='center'>About</Typography>
+
+            <Typography align='center'>
+                This is a project with the purpose of putting into practice the new knowledge that is learned about the study of <Link
+                    align='center'
+                    href='https://nextjs.org/'
+                    target='_blank'
+                    rel='noreferrer'
+                    color='secondary'>
+                    NextJS
+                </Link>. The idea is to avoid using external libraries and only use what <Link
+                    align='center'
+                    href='https://nextjs.org/'
+                    target='_blank'
+                    rel='noreferrer'
+                    color='secondary'>
+                    NextJS
+                </Link> already provides. Additionally, <Link
+                    align='center'
+                    href='https://mui.com/'
+                    target='_blank'
+                    rel='noreferrer'
+                    color='secondary'>
+                    MUI
+                </Link> is used for the visual part.
+            </Typography>
+
+            <Link
+                align='center'
+                href='https://www.ferb.ar/'
+                target='_blank'
+                rel='noreferrer'
+                color='secondary'>
+                Created by ferb.
+            </Link>
+        </Stack>
     )
 }
