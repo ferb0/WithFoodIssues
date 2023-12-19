f6c32b14fc8581782bb6d4b978efb6aafd37852b
ferb0 <ferb@e.email>
2023-12-19 08:38:06 -0300

    Se cambia nombre de variable url en get_data

--- src/app/results_list/_controllers/get_data.js
+++ src/app/results_list/_controllers/get_data.js
@@ -1,14 +1,14 @@
 import { objectToQuery, optionsControl } from './options.js'
 
 export async function getRecipes(search, options = {}, offSet = 0) {
-    let URL = `/api/search_recipes?ingredients=${search}&offSet=${offSet}`
+    let url = `/api/search_recipes?ingredients=${search}&offSet=${offSet}`
 
     // Se controla que las opciones sean las correctas.
     options = optionsControl(options)
 
     // Se convierte opciones enquery.
-    URL += objectToQuery(options)
+    url += objectToQuery(options)
 
-    let results = (await fetch(URL)).json()
+    let results = (await fetch(url)).json()
     return results
 }
